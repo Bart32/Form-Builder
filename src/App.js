220cb6a57bae40e4f4e4bebb461a3daf0b1a14fc
Bart32 <bartosz.kosiec@gmail.com>
2018-07-22 19:53:43 +0200

    0.2
    Wireframe is ready, creating form's left

--- src/App.js
+++ src/App.js
@@ -1,155 +1,201 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 
 
 class FormBuilder extends Component {
   constructor(){
     super()
+    this.addInput = this.addInput.bind(this);
+    this.state = {
+      
+      arrayOfInputs : [],
+      numberOfInputs : 0,
+      firstInput: true,
+      dynamicMarginLeft: -20,
+    }
+  }
+
+
+  addInput(){ 
+    this.setState (prevState => {
+      return{
+      numberOfInputs : prevState.numberOfInputs +1,
+      firstInput: true
+      }
+    });
+  }
+
+  render(){
+    for (let i = this.state.arrayOfInputs.length; i < this.state.numberOfInputs; i ++) {
+      this.state.arrayOfInputs.push(<ParentInput key={i} inputs={this.state.firstInput} dynamicMarginLeft={this.state.dynamicMarginLeft} handleChange={this.handleChange}/>);
+    };
+    return (
+        <div>
+        <h1 className="headerText"> Form Builder by Bartosz Kosiec </h1>
+          <div>
+            {this.state.arrayOfInputs}
+          </div>
+          <button type="submit" className="btn btn-primary addInputButton" onClick={this.addInput}>Add Input</button>
+        </div>
+    );
+  }
+};
+
+class ParentInput extends Component {
+  constructor(props){
+    super(props)
     this.addSubInput = this.addSubInput.bind(this);
     this.deleteInput = this.deleteInput.bind(this);
-    this.createForm = this.createForm.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    // this.resetValue = this.resetValue.bind(this);
     this.state = {
       arrayOfSubInputs : [],
       numberOfSubInputs : 0,
-      dynamicMarginLeft: 85,
-      nextQuestionCondition: 'textField' // NAZWA ROBOCZA DO ZMIANY 
+      hideInput: false,
+      dynamicMarginLeft: props.dynamicMarginLeft,
+      typeValue: 'text',
     }
   }
-  
+
+  componentDidMount(){
+    if(this.state.arrayOfSubInputs.length === 0){
+    this.setState (prevState => {
+      return{
+        dynamicMarginLeft : prevState.dynamicMarginLeft + 20
+      }
+    });
+  }
+}
+
   addSubInput(){
     this.setState (prevState => {
       return{
       numberOfSubInputs : prevState.numberOfSubInputs +1,
-      dynamicMarginLeft : 85 + (this.state.arrayOfSubInputs.length *10)
       }
+    });
+  }
+  
+  deleteInput() {  
+    this.setState({
+      hideInput: true
     });
   }
 
   handleChange(event){
     this.setState({
-      nextQuestionCondition: event.target.value
+      typeValue: event.target.value,
     });
 
   }
-
-deleteInput(event) {
-  const list = this.state.arrayOfSubInputs;
-  let indexElement = parseInt(event.target.id);
-  let howMany = this.state.arrayOfSubInputs.length - indexElement;
-  list.splice(indexElement, howMany );
-  this.setState(prevState => {
-    return{
-    arrayOfSubInputs: list,
-    numberOfSubInputs : prevState.numberOfSubInputs - howMany
-    }
-   });
-}
-createForm(){
-}
+  /*resetValue(){
+    this.setState({
+      typeValue: "text"
+    })
+  }
+*/
 
   render(){
+    alert(this.props.typeValue);
     for (let i = this.state.arrayOfSubInputs.length; i < this.state.numberOfSubInputs; i ++) {
-      this.state.arrayOfSubInputs.push(<SubInput key={i} index={i} addSubInput={this.addSubInput} dynamicMarginLeft={this.state.dynamicMarginLeft} deleteInput={this.deleteInput} nextQuestionCondition={this.nextQuestionCondition} />);
+      this.state.arrayOfSubInputs.push(<ParentInput key={i} resetValue={this.resetValue} dynamicMarginLeft={this.state.dynamicMarginLeft} typeValue={this.state.typeValue} />)
     };
-    return (
-      <div>
-      <h1 className="headerText"> Form Builder by Bartosz Kosiec </h1>
-        <div className="builder">
-          Question<input type="text" className="form-control"  placeholder="" />
-          Type <select className="form-control" onChange={this.handleChange}>
-            <option value='textField'>Text</option>
-            <option value='textField'>Number</option>
-            <option value="chooseField">Yes / No </option>
-          </select>
-          <button type="submit" className="btn btn-success" onClick={this.addSubInput}>Add Sub-Input</button>
-        </div>
+
+    let SubInputBuilderStyle = {
+      marginLeft: 50 + (this.props.dynamicMarginLeft),
+    };
+
+    let condition;
+    if(this.props.typeValue === 'number'){
+      condition = (
         <div>
-          {this.state.arrayOfSubInputs}
-        </div>
-        <button type="submit" className="btn btn-primary addInputButton" onClick={this.createForm}>Add Input</button>
-      </div>
-
-    );
-  }
-};
-
-class SubInput extends Component {
-  constructor(props){
-  super(props)
-  this.handleChange = this.handleChange.bind(this);
-  this.state = {
-    condition: 'oneOption',
-  }
-}
-handleChange(event){
-  this.setState({
-    condition: event.target.value
-  })
-  alert(this.state.condition)
-}
-  render() {
-    var SubInputBuilderStyle = {
-      marginLeft: this.props.dynamicMarginLeft + 'px'
-    };
-    let conditionOptions
-      if(this.state.condition === 'threeOptions'){
-        conditionOptions = (
+          <div className="form-group condition">
+          Condition
           <select className="form-control">
           <option>Equals</option>
           <option>Greater than</option>
           <option>Less than</option>
           </select>
-        )}
-        else(
-          conditionOptions = (
+        </div>
+        <div className="form-group condition">
+          <input type="text" className="form-control" />
+        </div>
+      </div>
+    )}
+    else if(this.props.typeValue === 'text'){
+      condition = (
+        <div>
+          <div className="form-group condition">
+          Condition
           <select className="form-control">
           <option>Equals</option>
           </select>
-          )
-        );
-      let conditionAnswerOptions
-        if(this.props.nextQuestionCondition === 'chooseField'){
-          conditionAnswerOptions = (
+        </div>
+        <div className="form-group condition">
+          <input type="text" className="form-control" />
+        </div>
+      </div>
+      )}
+      else{
+        condition = (
+          <div>
+            <div className="form-group condition">
+            Condition
             <select className="form-control">
-            <option>Yes</option>
-            <option>No</option>
+            <option>Equals</option>
             </select>
-          )}
-          else(
-            conditionAnswerOptions = (
-            <input type="text" className="form-control"  placeholder="" />
-            )
-          );
+          </div>
+          <div className="form-group condition">
+            <select className="form-control">
+              <option>Yes</option>
+              <option>No</option>
+            </select>
+          </div>
+        </div>
+        )}
     return (
+      this.state.hideInput ?
+      <div className='hidden'></div>
+      :
+      this.props.inputs ?
       <div>
-        <div className="SubInputBuilder" style={SubInputBuilderStyle}>
-        <div className="form-group condition">
-            Condition {conditionOptions}
+        <div className="firstInput">
+          Question<input type="text" className="form-control"  placeholder="" />
+          Type <select className="form-control" onChange={this.handleChange}>
+            <option value='text'>Text</option>
+            <option value='number'>Number</option>
+            <option value="radio">Yes / No </option>
+          </select>
+          <button type="submit" className="btn btn-success" onClick={this.addSubInput}>Add Sub-Input</button>
+          <button type="submit" className="btn btn-danger" onClick={this.deleteInput}>Delete</button>
         </div>
-        <div className="form-group condition">
-        {conditionAnswerOptions}
+        <div>
+          {this.state.arrayOfSubInputs}
         </div>
+      </div>
+      :
+      <div>
+        <div className="subInputBuilder" style={SubInputBuilderStyle}>
+            {condition}
           <div className="form-group">
             Question<input type="text" className="form-control"  placeholder="" />
           </div>
           <div className="form-group">
             Type <select className="form-control" onChange={this.handleChange}>
-              <option value='oneOption'>Text</option>
-              <option value='threeOptions'>Number</option>
-              <option value='oneOption'>Yes / No </option>
+              <option value='text'>Text</option>
+              <option value='number'>Number</option>
+              <option value='radio'>Yes / No </option>
             </select>
           </div>
-            <button type="submit" className="btn btn-primary" onClick={this.props.addSubInput}>Add Sub-Input</button>
-            <button type="submit" className="btn btn-danger"  onClick={this.props.deleteInput} id={this.props.index}>Delete</button>
+            <button type="submit" className="btn btn-success" onClick={this.addSubInput}>Add Sub-Input</button>
+            <button type="submit" className="btn btn-danger"  onClick={this.deleteInput }>Delete</button>
           </div>
+          <div>
+          {this.state.arrayOfSubInputs}
+        </div>
         </div>
     );
   }
-}
-
+};
 
 class App extends Component {
   render() {
